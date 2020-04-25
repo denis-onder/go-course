7961553f1fd3960600b1755386d47d60849acc8a
Denis Onder <denis_onder@protonmail.com>
2020-04-25 20:07:45 +0200

    Adding/removing to maps

--- maps/main.go
+++ maps/main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	// colors := make(map[string]string) => Creating maps with the make function
 	colors := map[string]string{
 		"red":   "FF0000",
 		"green": "00FF00",
 		"blue":  "0000FF",
 	}
+	colors["white"] = "FFFFFF" // Add pair to map
+	delete(colors, "blue")     // Remove pair from map via key
 	fmt.Println(colors)
 	// Iterate over map
 	for k, v := range colors {
