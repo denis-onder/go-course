ba102f2998f7bd35d1bad0eaf787a9688fc3764c
Denis Onder <denis_onder@protonmail.com>
2020-04-25 16:17:10 +0200

    Structs within a struct

--- structs/main.go
+++ structs/main.go
@@ -2,12 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email       string
+	phoneNumber int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	p := person{"John", "Doe"}
-	fmt.Println("Hello, " + p.firstName + " " + p.lastName)
+	contact := contactInfo{email: "test@mail.com", phoneNumber: 15555355}
+
+	p := person{
+		firstName: "John",
+		lastName:  "Doe",
+		contact:   contact,
+	}
+
+	fmt.Printf("%+v\n", p)
 }
